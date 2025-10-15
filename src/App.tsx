ded37e5232c73628c02d6a5db6f45730c4b791f4
Labswap <rafaellopezfdez85@gmail.com>
2025-10-15 18:46:50 +0100

    pdf

--- src/App.tsx
+++ src/App.tsx
@@ -184,7 +184,6 @@ Para más información de como inscribirte al club, no dudes en escribirnos un e
                   Descargar Reglamento
                 </a>
               </div>
-              <div style="width: 100%;"><div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;"><iframe title="Guia del corredor SIERRANEVADATRAILS" frameborder="0" width="1200" height="675" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genially.com/68d90ae68e5b6a684f5a3ac2" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> </div> </div>
             </div>
           </div>
 
